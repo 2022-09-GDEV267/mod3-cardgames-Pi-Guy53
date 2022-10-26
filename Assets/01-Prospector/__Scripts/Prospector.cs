41c7aeaf680166d9aa20b75d398a25957e692772
MJV314 <71029215+Pi-Guy53@users.noreply.github.com>
2022-10-26 19:13:38 -0400

    added win/lose conditions

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -227,6 +227,46 @@ public class Prospector : MonoBehaviour
 				SetTableauFaces();
 				break;
         }
+
+		CheckForGameOver();
+    }
+
+	void CheckForGameOver()
+    {
+		if (tableau.Count == 0)
+        {
+			GameOver(true);
+			return;
+        }
+
+		if(drawPile.Count > 0)
+        {
+			return;
+        }
+
+		foreach(CardProspector cd in tableau)
+        {
+			if(AdjacentRank(cd, target))
+            {
+				return;
+            }
+        }
+
+		GameOver(false);
+    }
+
+	void GameOver(bool won)
+    {
+		if(won)
+        {
+			print("you won");
+        }
+        else
+        {
+			print("you lost");
+        }
+
+		SceneManager.LoadScene("__Prospector");
     }
 
 	public bool AdjacentRank(CardProspector c0, CardProspector c1)

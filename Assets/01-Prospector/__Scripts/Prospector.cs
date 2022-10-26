7656152ce9246972f7131b39adb28b7f9aa34af4
MJV314 <71029215+Pi-Guy53@users.noreply.github.com>
2022-10-26 08:30:59 -0400

    added the layout script to parse the xml layout

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -5,33 +5,40 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-public class Prospector : MonoBehaviour {
-
-	static public Prospector 	S;
+public class Prospector : MonoBehaviour
+{
+	static public Prospector S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
+	public TextAsset deckXML;
+	public TextAsset layoutXML;
 
 
 	[Header("Set Dynamically")]
-	public Deck					deck;
+	public Deck deck;
+	public Layout layout;
 
-	void Awake(){
+	void Awake()
+	{
 		S = this;
 	}
 
-	void Start() {
-		deck = GetComponent<Deck> ();
-		deck.InitDeck (deckXML.text);
+	void Start()
+	{
+		deck = GetComponent<Deck>();
+		deck.InitDeck(deckXML.text);
 
 		Deck.Shuffle(ref deck.cards);
 
-		Card c;
-		for(int cNum =0; cNum<deck.cards.Count; cNum++)
-        {
+		/*Card c;
+		for (int cNum = 0; cNum < deck.cards.Count; cNum++)
+		{
 			c = deck.cards[cNum];
 			c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
-        }
+		} */
+
+		layout = GetComponent<Layout>();
+		layout.ReadLayout(layoutXML.text);
 	}
 
 }
